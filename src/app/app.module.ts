7fdfcf9d6a7a5029ddeb84163492ec95ca7c6111
Alondra <alobuuls@gmail.com>
2026-06-11 08:35:25 -0500

    ✨ feat: Add 404 page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from '@pages/404/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NotFoundComponent],
+  imports: [BrowserModule, RouterModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/404/not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css'],
+})
+export class NotFoundComponent {
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+}
