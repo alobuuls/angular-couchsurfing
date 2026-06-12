bebdcde6c75cea98a46cb130959dc058ff277862
Alondra <alobuuls@gmail.com>
2026-06-11 19:42:06 -0500

    ✨ feat(guests): Implement guests component and data rendering
    - add GuestsComponent
    - integrate CouchsurfingService data retrieval
    - handle loading, error and success states
    - display guests information from API
    - add initial guests page styles

--- src/app/pages/couchsurfing/guests/guests.component.ts
+++ src/app/pages/couchsurfing/guests/guests.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// Services
+import { CouchsurfingService } from '@services/couchsurfing.service';
+import { ErrorHandlerService } from '@services/err-handler.service';
+
+// Helpers
+import { withReqState } from '@helpers/with-state-operator';
+
+// Interfaces
+import { IGuests } from '@interfaces/couchsurfing.interface';
+import { DataState } from '@interfaces/data-structure-api';
 
 @Component({
   selector: 'guests',
@@ -6,7 +18,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guests.component.css'],
 })
 export class GuestsComponent implements OnInit {
-  constructor() {}
+  vm$!: Observable<DataState<IGuests[]>>;
 
-  ngOnInit(): void {}
+  constructor(
+    private _cs: CouchsurfingService,
+    private _errH: ErrorHandlerService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllGuests();
+  }
+
+  getAllGuests(): void {
+    this.vm$ = withReqState(this._cs.getAllGuests(), this._errH);
+  }
 }
