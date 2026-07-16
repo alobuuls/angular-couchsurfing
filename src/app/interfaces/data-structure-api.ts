f920ac9ed4f188f24397db32bc93f5b25bc56080
Alondra <alobuuls@gmail.com>
2026-07-16 13:55:16 -0500

    ♻️ refactor: Redesign request state models

--- src/app/interfaces/data-structure-api.ts
+++ src/app/interfaces/data-structure-api.ts
@@ -1,6 +1,48 @@
+import { IApiCsPag, IGuest, IGuestListItem } from '@interfaces/couchsurfing.interface';
+
 export interface IErrResp {
   message: string;
   status?: number;
 }
 
-export type DataState<T> = { status: 'loading'; data: null; error: null } | { status: 'success'; data: T; error: null } | { status: 'error'; data: null; error: IErrResp };
+export type ReqStatus = 'loading' | 'success' | 'error';
+
+export interface DataState<T> {
+  status: ReqStatus;
+  data: T;
+  error?: IErrResp;
+  pagination?: IApiCsPag;
+}
+
+export interface GuestsVM {
+  status: ReqStatus;
+  data: IGuest[];
+  pagination?: IApiCsPag;
+  offset: number;
+  error?: IErrResp;
+}
+
+
+export type IGuestTableRow = IGuestListItem & {
+  fullNames: string[];
+  genders: string[];
+  ages: (string | null)[];
+  continents: string[];
+
+  hometowns: { code?: string | null; city?: string | null }[];
+  livingIns: { code?: string | null; city?: string | null }[];
+
+  isHometownUnique: boolean;
+  isLivingInUnique: boolean;
+
+  ratings: number[];
+  isRatingUnique: boolean;
+
+  pageIndex?: number;
+};
+
+export type IGuestsTableVM = Omit<GuestsVM, 'data'> & {
+  data: IGuestTableRow[];
+};
+
+export type DataStatePag<T> = Pick<DataState<T>, 'data'> & IApiCsPag;
