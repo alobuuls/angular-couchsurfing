d5b8bcbc409c9ffbc853ffdd9700060680304ce3
Alondra <alobuuls@gmail.com>
2026-09-09 11:58:20 -0500

    🚀 ci: Configure GitHub Pages deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  endpointUrlApiCs: 'https://works-dana-yarn-fragrance.trycloudflare.com/api/v1',
+  endpointUrlApiCs: 'https://backend-api-cs-6038.onrender.com/api/v1',
   exampleEndpoint400: 'https://httpbin.org/status/400',
   exampleEndpoint404: 'https://httpbin.org/status/404',
   exampleEndpoint401: 'https://httpbin.org/status/401',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  endpointUrlApiCs: 'https://works-dana-yarn-fragrance.trycloudflare.com/api/v1',
+  endpointUrlApiCs: 'https://backend-api-cs-6038.onrender.com/api/v1',
   exampleEndpoint400: 'https://httpbin.org/status/400',
   exampleEndpoint404: 'https://httpbin.org/status/404',
   exampleEndpoint401: 'https://httpbin.org/status/401',
